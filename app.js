053cf1cde97b7c00d266cc3999395c0f7772812f
Diego <drbv27@gmail.com>
2021-10-31 15:31:35 -0500

    CRUD Create

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
 
 require("dotenv").config();
 
--- routers/Mascotas.js
+++ routers/Mascotas.js
@@ -16,4 +16,22 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/crear", (req, res) => {
+  res.render("crear");
+});
+
+router.post("/", async (req, res) => {
+  const body = req.body;
+  try {
+    /* const mascotaDB = new Mascota(body);
+    await mascotaDB.save(); */
+
+    await Mascota.create(body);
+
+    res.redirect("/mascotas");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
