697b2ea96ae11bfb6c5bd2e03ddff912c3e41491
Diego <drbv27@gmail.com>
2021-11-01 09:32:58 -0500

    CRUD delete

--- routers/Mascotas.js
+++ routers/Mascotas.js
@@ -51,4 +51,24 @@ router.get("/:id", async (req, res) => {
     });
   }
 });
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const mascotaDB = await Mascota.findByIdAndDelete({ _id: id });
+    if (mascotaDB) {
+      res.json({
+        estado: true,
+        mensaje: "Eliminado!!!",
+      });
+    } else {
+      res.json({
+        estado: false,
+        mensaje: "Fallo Eliminar!!!",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = router;
